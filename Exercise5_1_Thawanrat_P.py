bbd96e86399b2e5df04c0972f9383bbb842f3e37
Jadex <157593825+JadexDel@users.noreply.github.com>
2024-08-12 19:42:47 +0700

    Create Exercise5_1_Thawanrat_P.py

---
+++ Exercise5_1_Thawanrat_P.py
@@ -0,0 +1,15 @@
+# Input
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+# Process
+plus = num1 + num2
+minus = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+# Output
+print(num1, "+", num2, "=", plus)
+print(num1, "-", num2, "=", minus)
+print(num1, "*", num2, "=", multiply)
+print(num1, "/", num2, "=", divide)
